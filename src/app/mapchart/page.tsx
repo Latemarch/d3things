62bbff3e2c5220909a027aa47d849d533c42b0a8
Latemarch <73418661+Latemarch@users.noreply.github.com>
2023-09-25 22:44:43 +0900

    add recoil on silder

--- src/app/mapchart/page.tsx
+++ src/app/mapchart/page.tsx
@@ -1,28 +1,28 @@
-import D3Map from "@/components/D3Map";
-import D3Slider from "@/components/D3Slider";
-import { FeatureCollection } from "geojson";
+import D3Map from '@/components/D3Map'
+import D3Slider from '@/components/D3Slider'
+import { FeatureCollection } from 'geojson'
 
-const baseUrl = process.env.SERVER_BASEURL!!;
+const baseUrl = process.env.SERVER_BASEURL!!
 export default async function page() {
   // const mapData = await getMap("world.geo.json");
-  const mapData: FeatureCollection = await fetch(
-    baseUrl + `future-population/polygon/sido/d`
-  )
-    .then((res) => res.json())
-    .then((result) => convertToGeoJSON(result) as FeatureCollection);
-  console.log(mapData.features[0]);
+  // const mapData: FeatureCollection = await fetch(
+  //   baseUrl + `future-population/polygon/sido/d`
+  // )
+  //   .then((res) => res.json())
+  //   .then((result) => convertToGeoJSON(result) as FeatureCollection);
+  // console.log(mapData.features[0]);
 
   return (
     <div className="p-4">
-      <D3Map mapData={mapData} height={600} />
+      {/* <D3Map mapData={mapData} height={600} /> */}
       <D3Slider />
     </div>
-  );
+  )
 }
 
 function convertToGeoJSON(data: any) {
   return {
-    type: "FeatureCollection",
+    type: 'FeatureCollection',
     features: data.map((item: any) => item.alias1.feature),
-  };
+  }
 }
--- src/components/D3Slider.tsx
+++ src/components/D3Slider.tsx
@@ -1,112 +1,138 @@
-"use client";
+'use client'
 
-import { useEffect, useRef } from "react";
-import * as d3 from "d3";
-import { useRecoilState } from "recoil";
-import { LivingSliderPoint } from "@/context/atom";
+import { useEffect, useRef } from 'react'
+import * as d3 from 'd3'
+import { useRecoilState } from 'recoil'
+import { LivingSliderPoint } from '@/context/atom'
+import { PiMinusSquareLight, PiPlusSquareLight } from 'react-icons/pi'
 
 interface Props {
-  data?: any;
-  height?: number;
-  width?: number;
+  data?: any
+  height?: number
+  width?: number
 }
 
-export default function D3Slider({ data, height = 30, width = 200 }: Props) {
-  const ref = useRef<HTMLDivElement | null>(null);
-  const [sliderPoint, setSliderPoint] = useRecoilState(LivingSliderPoint);
+export default function D3Slider({ data, height = 40, width = 200 }: Props) {
+  const ref = useRef<HTMLDivElement | null>(null)
+  const [sliderPoint, setSliderPoint] = useRecoilState(LivingSliderPoint)
 
   useEffect(() => {
-    if (!ref.current) return;
+    if (!ref.current) return
 
     //Remove any existing SVG
-    d3.select(ref.current).selectAll("svg").remove();
+    d3.select(ref.current).selectAll('svg').remove()
 
     const svg1 = d3
       .select(ref.current)
-      .append("svg")
-      .attr("width", width + 20)
-      .attr("height", height)
-      .style("border", "1px solid steelblue");
+      .append('svg')
+      .attr('width', width + 20)
+      .attr('height', height)
+    // .style('border', '1px solid steelblue')
     const svg = svg1
-      .append("svg")
-      .append("g")
-      .attr("width", width)
-      .attr("height", height - 20)
-      .attr("transform", "translate(10,5)")
-      .style("border", "1px solid red");
+      .append('svg')
+      .append('g')
+      .attr('width', width)
+      .attr('height', height - 20)
+      .attr('transform', 'translate(10,5)')
+    // .style('border', '1px solid red')
 
-    const xScale = d3.scaleLinear([10, 100], [0, width]);
+    const xScale = d3.scaleLinear([10, 100], [0, width])
 
-    const xAxis = d3.axisBottom(xScale);
+    const xAxis = d3.axisBottom(xScale)
 
     const xAxisG = svg
-      .append("g")
-      // .attr("transform", "translate(0,20)")
-      .call(xAxis);
+      .append('g')
+      .attr('transform', 'translate(0,7)')
+      .call(xAxis)
 
-    xAxisG.selectAll("line").style("stroke", "none");
-    xAxisG.selectAll("path").style("stroke", "none");
+    xAxisG.selectAll('line').style('stroke', 'none')
+    xAxisG.selectAll('path').style('stroke', 'none')
 
     svg
-      .append("rect")
-      .attr("width", xScale(100))
-      .attr("height", 5)
-      .attr("fill", "#d9d9d9");
+      .append('rect')
+      .attr('width', xScale(100))
+      .attr('height', 5)
+      .attr('fill', '#d9d9d9')
 
     const bar = svg
-      .append("rect")
-      .attr("class", "dataBar")
-      .attr("width", xScale(20))
-      .attr("height", 5)
-      .attr("fill", "#1C72BF");
+      .append('rect')
+      .attr('class', 'dataBar')
+      .attr('width', xScale(20))
+      .attr('height', 5)
+      .attr('fill', '#1C72BF')
 
     const initialCircle = svg
-      .selectAll(".boundaryCircle")
+      .selectAll('.boundaryCircle')
       .data([10, 100])
       .enter()
-      .append("circle")
-      .attr("cx", (d) => xScale(d))
-      .attr("cy", 2.5)
-      .attr("r", 2.5)
-      .attr("fill", (d) => (d < 100 ? "#1C72BF" : "#d9d9d9"));
+      .append('circle')
+      .attr('cx', (d) => xScale(d))
+      .attr('cy', 2.5)
+      .attr('r', 2.5)
+      .attr('fill', (d) => (d < 100 ? '#1C72BF' : '#d9d9d9'))
 
     const circle = svg
-      .selectAll(".datacircle")
+      .selectAll('.datacircle')
       .data([20])
       .enter()
-      .append("circle")
-      .attr("cx", (d) => xScale(d))
-      .attr("cy", 2.5)
-      .attr("r", 5)
-      .attr("stroke", "white")
-      .attr("fill", "#6FADE4");
+      .append('circle')
+      .attr('cx', (d) => xScale(d))
+      .attr('cy', 2.5)
+      .attr('r', 5)
+      .attr('stroke', 'white')
+      .attr('fill', '#6FADE4')
 
     const drag = d3
       .drag<SVGCircleElement, number, number>()
-      .on("drag", dragging);
+      .on('drag', dragging)
+
     function dragging(
       this: SVGGElement,
-      e: d3.D3DragEvent<SVGGElement, number, number>
+      e: d3.D3DragEvent<SVGGElement, number, number>,
     ) {
-      const maxX = Math.max(0, Math.min(width, e.x));
-      const newX = Math.min(maxX, width - 0);
+      const quantizeScale = d3
+        .scaleQuantize()
+        .domain([0, width])
+        .range(d3.range(10, 101, 1))
+      const quantizedValue = quantizeScale(e.x)
 
-      // d3.select(this).attr("transform", `translate(${xScale(newX)},2.5)`);
-      d3.select(this).attr("cx", newX);
-      svg.select(".dataBar").attr("width", newX);
-      console.log(newX, xScale(newX));
-      setSliderPoint(xScale.invert(newX));
+      const newX = xScale(quantizedValue)
+      d3.select(this).attr('cx', newX)
+      svg.select('.dataBar').attr('width', newX)
+      setSliderPoint(xScale.invert(newX))
     }
-    circle.call(drag);
+    circle.call(drag)
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [])
 
+  const handleClick = (type: string) => {
+    if (type === 'minus') {
+      if (sliderPoint % 10 === 0) {
+        setSliderPoint(sliderPoint - 10)
+      } else {
+        setSliderPoint(sliderPoint - (sliderPoint % 10))
+      }
+    } else if (type === 'plus') {
+      if (sliderPoint % 10 === 0) {
+        setSliderPoint(sliderPoint + 10)
+      } else {
+        setSliderPoint(sliderPoint + (10 - (sliderPoint % 10)))
+      }
+    }
+  }
   return (
-    <div className="flex ">
-      <div>a</div>
+    <div className="flex text-2xl">
+      <PiMinusSquareLight
+        className="cursor-pointer"
+        onClick={() => handleClick('minus')}
+      />
+
       <div ref={ref}></div>
-      <div>b</div>
+      <PiPlusSquareLight
+        className="cursor-pointer"
+        onClick={() => handleClick('plus')}
+      />
       {sliderPoint}
     </div>
-  );
+  )
 }
