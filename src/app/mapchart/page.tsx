1e58f055801904c309b83ce36462b267cc2beb51
LateMarch <73418661+Latemarch@users.noreply.github.com>
2023-10-06 16:57:49 +0900

    add d3kakao

--- src/app/mapchart/page.tsx
+++ src/app/mapchart/page.tsx
@@ -1,14 +1,15 @@
-import D3Map from '@/components/D3Map'
-import D3PopulationPyramid from '@/components/D3PopulationPyramid'
-import D3Slider from '@/components/D3Slider'
-import KakaoMap from '@/components/KakaoMap'
-import { getMap } from '@/service/server'
-import { FeatureCollection } from 'geojson'
+import D3Map from "@/components/D3Map";
+import D3PopulationPyramid from "@/components/D3PopulationPyramid";
+import D3Slider from "@/components/D3Slider";
+import D3KakaoMap from "@/components/D3kakaomap";
+import KakaoMap from "@/components/KakaoMap";
+import { getMap } from "@/service/server";
+import { FeatureCollection } from "geojson";
 
-const baseUrl = process.env.SERVER_BASEURL!!
-const appKey = process.env.KAKAO_MAP_API!!
+const baseUrl = process.env.SERVER_BASEURL!!;
+const appKey = process.env.KAKAO_MAP_API!!;
 export default async function page() {
-  const mapData = await getMap('world.geo.json')
+  const mapData = await getMap("korea.geojson");
   // const mapData: FeatureCollection = await fetch(
   //   baseUrl + `future-population/polygon/sido/d`
   // )
@@ -18,16 +19,14 @@ export default async function page() {
 
   return (
     <div className="p-4">
-      <D3Map mapData={mapData} height={600} />
-      {/* <D3Slider /> */}
-      {/* <KakaoMap /> */}
+      <D3KakaoMap />
     </div>
-  )
+  );
 }
 
 function convertToGeoJSON(data: any) {
   return {
-    type: 'FeatureCollection',
+    type: "FeatureCollection",
     features: data.map((item: any) => item.alias1.feature),
-  }
+  };
 }
--- src/components/D3kakaomap.tsx
+++ src/components/D3kakaomap.tsx
@@ -1,8 +1,10 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 "use client";
 
 import { useEffect, useRef, useState } from "react";
 import * as d3 from "d3";
 import { KakaoMapLoader, zoomLevel, zoomSVGOnKakaoMap } from "@/service/client";
+import { Feature } from "geojson";
 
 interface Props {
   data?: any;
@@ -10,22 +12,49 @@ interface Props {
   width?: number;
 }
 
-export default function D3KakaoMap({ data, width = 800, height = 500 }: Props) {
+export default function D3KakaoMap({ data }: Props) {
   const ref = useRef<HTMLDivElement | null>(null);
-  const [map, setMap] = useState<any>();
+  const [map, setMap] = useState<any>(undefined);
   const initialZoomLevel = 9;
 
+  const center = d3.geoCentroid(data);
+  // const projection = d3.geoMercator().fitWidth(500, data);
+  // const geoGenerator = d3.geoPath().projection(projection);
+
+  //kakao Map load
   useEffect(() => {
+    console.log(center);
     KakaoMapLoader({
       setMap: setMap,
-      location: [37.5642, 127.00169],
+      // location: [center[1], center[0]],
       otherOptions: { level: initialZoomLevel },
     });
-    if (!ref.current) return;
-    console.log("what");
-    console.log(ref.current);
+  }, []);
 
-    //Remove any existing SVG
+  useEffect(() => {
+    if (!map || !window.kakao) return;
+
+    // kakao.maps.event.addListener(map, "click", (e: any) => {
+    //   console.log(e.latLng);
+    // });
+
+    // kakao.maps.event.addListener(map, "zoom_changed", () =>
+    //   zoomSVGOnKakaoMap({
+    //     map,
+    //     allSVG: d3.select(".body").selectAll("svg"),
+    //     initialZoomLevel,
+    //   })
+    // );
+    const mapProjection = map.getProjection();
+    const mapBounds = map.getBounds();
+    const mapSW = mapBounds.getSouthWest();
+    const mapNE = mapBounds.getNorthEast();
+    const SWCoords = mapProjection.pointFromCoords(mapSW);
+    const NECoords = mapProjection.pointFromCoords(mapNE);
+    const width = NECoords.x;
+    const height = SWCoords.y;
+
+    if (!ref.current) return;
     d3.select(ref.current).selectAll("svg").remove();
 
     const svg = d3
@@ -35,56 +64,58 @@ export default function D3KakaoMap({ data, width = 800, height = 500 }: Props) {
       .attr("width", width)
       .attr("height", height)
       .style("border", "1px solid steelblue")
+      .style("opacity", "0.5")
       .append("g");
 
-    const line = d3
-      .line()
-      .x((d) => d[0])
-      .y((d) => d[1]);
+    // const projection = d3.geoMercator().fitWidth(580, data);
+    // console.log(data);
+    // console.log(projection(data.features[0]));
+    // const geoGenerator = d3.geoPath().projection(projection);
+    // const [x, y] = projection(center) as Array<number>;
 
-    svg
-      .append("rect")
-      .attr("id", "rect1")
-      .attr("width", 100)
-      .attr("height", 100);
+    // const mapPolygon = svg
+    //   .selectAll("path")
+    //   .append("g")
+    //   .attr("class", "map1")
+    //   .data(data.features)
+    //   .enter()
+    //   .append("path")
+    //   .attr("d", (d: any) => geoGenerator(d))
+    //   .attr("fill", "#69b3a2")
+    //   .attr("stroke", "white")
+    //   .attr("transform", `translate(${width / 2 - x},${height / 2 - y - 2})`)
+    //   .on("mouseover", function (e, d) {
+    //     d3.select(this).attr("fill", "#cb7070");
+    //   })
+    //   .on("mouseleave", function () {
+    //     d3.select(this).attr("fill", "#69b3a2");
+    //   })
+    //   .on("click", function (e, d) {
+    //     // console.log(projection.invert([e.x, e.y]));
+    //   });
 
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-  useEffect(() => {
-    if (!map || !window.kakao) return;
-
-    const position = new window.kakao.maps.LatLng(37.5642, 127.00169);
-    const content = document.getElementsByClassName("svg")[0];
-    const custumOverlay = new window.kakao.maps.CustomOverlay({
-      position,
-      content,
-    });
-    custumOverlay.setMap(map);
-
-    kakao.maps.event.addListener(map, "click", (e: any) => {
-      addSqure({ e, map, initialZoomLevel });
-    });
-
-    kakao.maps.event.addListener(map, "zoom_changed", () =>
-      zoomSVGOnKakaoMap({
-        map,
-        allSVG: d3.select(".body").selectAll("svg"),
-        initialZoomLevel,
-      })
-    );
-    return () => {
-      kakao.maps.event.removeListener(map, "zoom_changed", () =>
-        zoomSVGOnKakaoMap({
-          map,
-          allSVG: d3.selectAll("svg"),
-          initialZoomLevel,
-        })
-      );
-    };
+    // const position = new window.kakao.maps.LatLng(center[1], center[0]);
+    // const content = document.getElementsByClassName("svg")[0];
+    // const custumOverlay = new window.kakao.maps.CustomOverlay({
+    //   position,
+    //   content,
+    // });
+    // custumOverlay.setMap(map);
+    // const projectionObj = map.getProjection();
+    // console.log(projectionObj);
+    // return () => {
+    //   kakao.maps.event.removeListener(map, "zoom_changed", () =>
+    //     zoomSVGOnKakaoMap({
+    //       map,
+    //       allSVG: d3.selectAll("svg"),
+    //       initialZoomLevel,
+    //     })
+    //   );
+    // };
   }, [map]);
 
   return (
-    <div>
+    <div className="body">
       <div ref={ref} className="svg" />
       <div
         id="map"
@@ -98,21 +129,34 @@ function addSqure({
   e,
   map,
   initialZoomLevel,
+  data,
+  geoGenerator,
 }: {
   e: any;
   map: any;
   initialZoomLevel: number;
+  data?: any;
+  geoGenerator: any;
 }) {
   const position = new window.kakao.maps.LatLng(e.latLng.Ma, e.latLng.La);
   const newDiv = document.createElement("div");
   const scale = zoomLevel[initialZoomLevel] / zoomLevel[map.getLevel()];
+  const width = 1000;
+  const height = 1000;
   document.body.appendChild(newDiv);
   d3.select(newDiv)
     .append("svg")
-    .attr("width", 100)
-    .attr("height", 100)
-    .attr("transform", `scale(${scale})`)
-    .style("border", "1px solid steelblue");
+    .attr("width", width)
+    .attr("height", height)
+    // .attr("transform", `scale(${scale})`)
+    // .style("border", "1px solid steelblue");
+    .append("g")
+    .attr("class", "map1")
+    .append("path")
+    .attr("d", geoGenerator(data))
+    .attr("fill", "#69b3a2")
+    .attr("stroke", "white");
+  // .attr("transform", `translate(${width / 2 - x},${height / 2 - y})`);
   const custumOverlay = new window.kakao.maps.CustomOverlay({
     position,
     content: newDiv,
